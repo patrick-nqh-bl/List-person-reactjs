b77c872246e97fe76df1030bb7b03570eac13a64
Patrick <patrick204nqh@gmail.com>
2020-04-05 13:33:12 +0700

    commit first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => (<PersonList />)
+
+const PersonList = () => {
+  const peoples = [
+    {
+      age: 22, name:'John', job: 'developer',
+      url: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+    },
+    {
+      age: 32, name:'John', job: 'developer',
+      url: "https://randomuser.me/api/portraits/thumb/men/70.jpg"
+    },
+    {
+      age: 22, name:'John', job: 'developer',
+      url: "https://randomuser.me/api/portraits/thumb/men/23.jpg"
+    }
+]
+const persons = peoples.map((people, index) =>{
+  return <Person person={people} key={index}/>;
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      {persons}
+    </section>
   );
 }
 
-export default App;
+const Person = (props) => {
+  return (
+    <div className="person">
+      <img src={props.person.url} alt="avt"/>
+      <div>
+        <h4>{props.person.name}</h4>
+        <h4>{props.person.job}</h4>
+        <h3>{props.person.age}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default App;
